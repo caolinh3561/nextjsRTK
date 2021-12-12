a501ec610e3cd64086d98dc14c42532b8871c926
caolinh3561 <caobalinh3561@gmail.com>
2021-12-12 13:38:55 +0700

    fix undefined

--- pages/imageComponent/index.js
+++ pages/imageComponent/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 
 export default function ImageComponent({post}) {
     // console.log(post);
+    if(!post) return <></>;
     return (
         <div style={{height:300,width:300, marginTop:40}}>
             <h2 style={{margin:0,marginBottom:10}}>{post.author}</h2>
