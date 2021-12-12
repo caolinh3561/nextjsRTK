6842e0d360c8f9a0b56921888b9838439022245d
caolinh3561 <caobalinh3561@gmail.com>
2021-12-12 14:21:56 +0700

    jira test text

--- pages/imageComponent/index.js
+++ pages/imageComponent/index.js
@@ -7,6 +7,7 @@ export default function ImageComponent({post}) {
     if(!post) return <></>;
     return (
         <div style={{height:300,width:300, marginTop:40}}>
+            <h1>hmmmm.... test text</h1>
             <h2 style={{margin:0,marginBottom:10}}>{post.author}</h2>
             <Image width="300" height="300" 
             src={post.download_url} alt={post.author} />
