f213f1a33a7dfba9fba8524c4552137ec914e3b6
caolinh3561 <caobalinh3561@gmail.com>
2021-12-12 13:26:56 +0700

    jira FNMS-hihi: use next/image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  Image:{
-    domain:["picsum.photos"],
-  }
+  images: {
+    domains: ['picsum.photos'],
+  },
 }
--- pages/imageComponent/index.js
+++ pages/imageComponent/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import Image from 'next/image'
 
 export default function ImageComponent({post}) {
     // console.log(post);
     return (
         <div style={{height:300,width:300, marginTop:40}}>
             <h2 style={{margin:0,marginBottom:10}}>{post.author}</h2>
-            <img width="300" height="300" 
+            <Image width="300" height="300" 
             src={post.download_url} alt={post.author} />
         </div>
     )
